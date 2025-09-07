e92fc539357d23c15998430ad37650796622ab71
Carlos Ribeiro <dev.carlosribeiro@gmail.com>
2025-09-07 01:16:29 -0300

    Deafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// FEITO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia ", "00000000000001", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Calculadora");
+Iphone iphone = new Iphone("987654321", "iPhone 15", "00000000000002", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
